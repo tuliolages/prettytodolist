587edb09d0cd005e5583efb0045f21db6331c1d4
Túlio Paulo Lages da Siva <tulio.plds@gmail.com>
2018-01-26 02:39:40 -0300

    fix: remove test task

--- users/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery.task.schedules import crontab
-from celery.decorators import periodic_task
-from celery.utils.log import get_task_logger
-
-logger = get_task_logger(__name__)
-
-@periodic_task(run_every=(crontab(minute='*/1')), name="some_task", ignore_result=True)
-def some_task():
-    logger.info("Updated Test task")
